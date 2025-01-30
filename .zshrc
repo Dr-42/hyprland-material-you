6b49e90962a70b2732e605a548a74d7fb5f8f23b
Dr-42 <royspandan1@gmail.com>
2025-01-30 19:26:12 +0530

    READY

---
+++ .zshrc
@@ -0,0 +1,47 @@
+fastfetch
+export ENABLE_CORRECTION="true"
+
+export EDITOR=nvim
+alias vim='nvim'
+alias ls='eza --icons --group-directories-first'
+alias cd='z'
+
+# Flatpak aliases
+alias bottles='flatpak run --env="GSK_RENDERER=ngl" com.usebottles.bottles'
+
+export LS_COLORS="$LS_COLORS:ow=1;34:tw=1;34:"
+
+export PATH="$PATH:/home/spandan/sdks/flutter/bin"
+export PATH="$PATH:/home/spandan/.local/bin"
+export PATH="$PATH:/usr/local/cuda/bin"
+export PATH="$PATH:/home/spandan/.cargo/bin"
+export PATH="$PATH:/home/spandan/.deno/bin"
+export CHROME_EXECUTABLE="chromium-browser"
+export ANDROID_HOME="/home/spandan/Android/Sdk"
+export NDK_HOME="$ANDROID_HOME/ndk/$(ls -1 $ANDROID_HOME/ndk)"
+export JAVA_HOME="/home/spandan/android-studio/jbr"
+
+export MANPAGER="nvim +Man!"
+
+HISTFILE=~/.zsh_history
+HISTSIZE=100000
+SAVEHIST=1000
+setopt appendhistory
+
+autoload -Uz compinit && compinit
+
+eval "$(starship init zsh)"
+eval "$(zoxide init zsh)"
+
+# Home, end and delete keys
+bindkey '\e[H'  beginning-of-line
+bindkey '\e[F'  end-of-line
+bindkey '\e[3~' delete-char
+
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+source /usr/share/fzf/key-bindings.zsh
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - zsh)"
--- sddm/scripts/wallpaper.sh
+++ sddm/scripts/wallpaper.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 cache_file="$HOME/.cache/current_wallpaper"
 if [ ! -d /etc/sddm.conf.d/ ]; then
-    sudo mkdir /etc/sddm.conf.d
-    echo "Folder /etc/sddm.conf.d created."
+  sudo mkdir /etc/sddm.conf.d
+  echo "Folder /etc/sddm.conf.d created."
 fi
 
 cp $HOME/dotfiles/sddm/sddm.conf /etc/sddm.conf.d/
 cp $HOME/dotfiles/sddm/sddm.conf /etc/
 if [ "$1" != "init" ]; then
-    echo "File /etc/sddm.conf.d/sddm.conf updated."
+  echo "File /etc/sddm.conf.d/sddm.conf updated."
 fi
 
 current_wallpaper=$(cat "$cache_file")
@@ -17,7 +17,7 @@ extension="${current_wallpaper##*.}"
 cp $current_wallpaper /usr/share/sddm/themes/corners/backgrounds/current_wallpaper.$extension
 
 if [ "$1" != "init" ]; then
-    echo "Current wallpaper copied into /usr/share/sddm/themes/corners/backgrounds/"
+  echo "Current wallpaper copied into /usr/share/sddm/themes/corners/backgrounds/"
 fi
 
 new_wall=$(echo $current_wallpaper | sed "s|$HOME/wallpaper/||g")
@@ -25,7 +25,7 @@ cp $HOME/.cache/material/colors-sddm-style.conf /usr/share/sddm/themes/corners/t
 sed -i 's/CURRENTWALLPAPER/'"current_wallpaper.$extension"'/' /usr/share/sddm/themes/corners/theme.conf
 
 if [ "$1" != "init" ]; then
-    echo "File theme.conf updated in /usr/share/sddm/themes/corners/"
+  echo "File theme.conf updated in /usr/share/sddm/themes/corners/"
 
-    echo "DONE! Please logout to test sddm."
+  echo "DONE! Please logout to test sddm."
 fi
